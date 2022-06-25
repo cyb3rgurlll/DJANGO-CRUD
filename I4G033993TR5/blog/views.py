f83e315b4faa442fcf336735e001dfb9e8e6d2ac
osasschima@yahoo.com <osasschima@yahoo.com>
2022-06-25 18:28:34 +0100

    Included the views

--- I4G033993TR5/blog/views.py
+++ I4G033993TR5/blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
+
+from I4G033993TR5.blog.models import Post
 
 # Create your views here.
+class PostListView(ListView):
+  model = Post
+class PostCreateView(CreateView):
+  model = Post
+  fields = "__all__"
+  success_url = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+  model = Post
+
+class PostDeleteView(UpdateView):
+  model = Post
+  fields = "__all__"
+  success_url = reverse_lazy("blog:all")
